c05bdc4aa7a4025114057fef5e5b345c24695a5b
rgervacio0803 <ogturtel@yahoo.com>
2025-07-21 11:06:42 -0700

    2nd one

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import LibraryLogo from '../assets/Library.svg'
 
 const Nav = () => {
     return (
     <nav>
       <div className="nav__container">
         <a href="/">
-        <img src="" alt="" className="logo" />
+        <img src={LibraryLogo} alt="" className="logo" />
         </a>
         <ul className='nav__links'>
             <li className='nav__list'>
